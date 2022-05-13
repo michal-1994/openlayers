a6d517942b6f11c2d58a3930679191206c59386a
Michal <michalgrzegorczyk04@gmail.com>
2022-05-13 17:41:18 +0200

    Base map configuration

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 4
+window.onload = init;
+
+function init () {
+    const map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: [2130866.237095212, 6495567.041331601],
+          zoom: 16,
+          maxZoom: 20,
+          minZoom: 10,
+          rotation: 0
+        })
+    });
+
+    map.on('click', function (e) {
+        console.log(e);
+        console.log(e.coordinate);
     })
-});
+}
